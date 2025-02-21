434042e31bf0ccaec3b9779b1a751bfe9c1bfda3
Oleksii Shyshkin <alexshyshkin7@gmail.com>
2025-02-22 01:27:30 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,7 +1,7 @@
 /*
-===============================================================================
+==================================================================================
 Stored Procedure: Load Silver Layer (Bronze -> Silver)
-===============================================================================
+==================================================================================
 Script Purpose:
     This stored procedure performs the ETL (Extract, Transform, Load) process to 
     populate the 'silver' schema tables from the 'bronze' schema.
@@ -15,7 +15,7 @@ Parameters:
 
 Usage Example:
     EXEC Silver.load_silver;
-===============================================================================
+==================================================================================
 */
 
 CREATE OR ALTER PROCEDURE silver.load_silver AS
@@ -24,13 +24,13 @@ BEGIN
 
     BEGIN TRY
         SET @batch_start_time = GETDATE();
-        PRINT '================================================';
+        PRINT '===================================================';
         PRINT 'Loading Silver Layer';
-        PRINT '================================================';
+        PRINT '===================================================';
 
-        PRINT '------------------------------------------------';
+        PRINT '-------------------------------------------';
         PRINT 'Loading CRM Tables';
-        PRINT '------------------------------------------------';
+        PRINT '-------------------------------------------';
 
         -- Loading silver.crm_cust_info
         SET @start_time = GETDATE();
@@ -73,7 +73,7 @@ BEGIN
 
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+        PRINT '>>-----------------------------------------';
 
         -- Loading silver.crm_prd_info
         SET @start_time = GETDATE();
@@ -110,7 +110,7 @@ BEGIN
 
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+        PRINT '>>-----------------------------------------';
 
         -- Loading silver.crm_sales_details
         SET @start_time = GETDATE();
@@ -160,11 +160,11 @@ BEGIN
 
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+        PRINT '>>-----------------------------------------';
 
-        PRINT '------------------------------------------------';
+        PRINT '-------------------------------------------';
         PRINT 'Loading ERP Tables';
-        PRINT '------------------------------------------------';
+        PRINT '-------------------------------------------';
 
         -- Loading silver.erp_cust_az12
         SET @start_time = GETDATE();
@@ -195,23 +195,23 @@ BEGIN
 
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-        PRINT '>> -------------';
+        PRINT '>>-----------------------------------------';
 
         -- Final batch completion log
         SET @batch_end_time = GETDATE();
-        PRINT '==========================================';
+        PRINT '===========================================';
         PRINT 'Loading Silver Layer Completed';
         PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-        PRINT '==========================================';
+        PRINT '===========================================';
 
     END TRY
 
     BEGIN CATCH
-        PRINT '==========================================';
+        PRINT '===========================================';
         PRINT 'ERROR OCCURRED DURING LOADING SILVER LAYER';
         PRINT 'Error Message: ' + ERROR_MESSAGE();
         PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR);
         PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR);
-        PRINT '==========================================';
+        PRINT '===========================================';
     END CATCH
 END;
